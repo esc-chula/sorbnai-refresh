a881944f80689e02f34ac7d84d424771093c3866
shalluv <p.kunlayanathee@gmail.com>
2025-11-20 09:36:34 +0700

    feat: init

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+//  @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+}
+
+export default config
