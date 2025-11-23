e0e62363e1509ef73d580c5320f571cec288b02b
shalluv <p.kunlayanathee@gmail.com>
2025-11-23 14:19:15 +0700

    fix: spamming welcome modal

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -65,7 +65,7 @@ function App() {
       setStoredExams(exams)
       setWelcomeOpen(true)
     }
-  }, [exams, storedId, setStoredExams, storedExams])
+  }, [])
 
   const form = useForm({
     defaultValues: {
